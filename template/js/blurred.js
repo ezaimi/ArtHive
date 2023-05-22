eeff1a1b58807f6874b1f4a2a46a238019c777d6
erisa zaimi <ezaimi21@epoka.edu.al>
2023-05-22 18:27:12 +0200

    login added

--- template/js/blurred.js
+++ template/js/blurred.js
@@ -1,4 +1,17 @@
 function toggle() {
     var blur = document.getElementById('blur');
     blur.classList.toggle('active');
+
+    /*popup-i*/
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+function toggle2() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    /*popup-i*/
+    var popup = document.getElementById('popup2');
+    popup.classList.toggle('active');
 }
---
+++ template/js/loginUI.js
@@ -0,0 +1,12 @@
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+
+const popup = document.getElementById("popup");
+
+registerButton.addEventListener("click", () => {
+    popup.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+    popup.classList.remove("right-panel-active");
+});
