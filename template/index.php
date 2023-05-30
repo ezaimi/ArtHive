5d7b76f66b9450852fe8128a78557ab8400dc06f
hklea <khaxhiu21@epoka.edu.al>
2023-05-30 23:55:25 +0200

    Section

--- template/index.php
+++ template/index.php
@@ -434,7 +434,7 @@
 
 
 
-    <!--Wave Shape-->
+    <!--  Klea-->
 
     <!-- <div class="custom-shape-divider-bottom-1684688853">
       <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
@@ -443,13 +443,38 @@
           class="shape-fill"></path>
       </svg>
     </div> -->
-    <div class="custom-shape-divider-bottom-1684688853">
+    <!-- <div class="custom-shape-divider-bottom-1684688853">
   <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
     <path
       d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
       class="shape-fill"></path>
   </svg>
-</div>
+</div> -->
+<section>
+<!-- <div class="custom-shape-divider-bottom-1684688853">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        -- <path
+          d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+          class="shape-fill" ></path> ->
+          <path d="M 985.66 92.83 C 906.67 72 823.78 31 743.84 14.19 c -82.26 -17.34 -168.06 -16.33 -250.45 0.39 c -57.84 11.73 -114 31.07 -172 41.86 A 600.21 600.21 0 0 1 0 27.35 V 120 H 1200 V 95.8 C 1132.19 118.92 1055.71 111.31 985.66 92.83 Z" fill="#292929"/>
+
+      </svg> -->
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class=single-cat mb-50 wow fadeInUp data-wow-duration="1s" data-wow-delay=".2s"
+            style="visibility: visible; animation-duration: 1s; animation-delay: 0.2s; animation-name: fadeInUp;">
+            </div>
+          </div>
+          
+        </div>
+  
+      </div>
+    </div>
+    
+
+
+</section>
 
 
 
